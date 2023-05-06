07af87f5636e269e1cbde7621a6dc8c3b57e58e3
Feiron <pawel.rogowski.praca@gmail.com>
2023-05-07 00:30:32 +0200

    added time travel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Cell } from "./components/Cell/Cell";
 import { ResetButton } from "./components/ResetButton/ResetButton";
+import { GoBackButton } from "./components/GoBackButton/GoBackButton";
 import { useState } from "react";
 
 // In App.js
@@ -8,22 +9,78 @@ import { useState } from "react";
 export default function App() {
   const [nextSymbol, setNextSymbol] = useState("❌");
   const [cells, setCells] = useState(Array(9).fill(null));
+  const [winner, setWinner] = useState(null);
+  const [history, setHistory] = useState([]);
+  const [currentMove, setCurrentMove] = useState(0);
 
-  const resetBoard = () => setCells(Array(9).fill(null));
+  const handleResetBoard = () => {
+    setNextSymbol("❌");
+    setWinner(null);
+    setCells(Array(9).fill(null));
+  };
+  const winningCombinations = [
+    // horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // diagonal
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const calculateWinner = (cells) => {
+    for (let i = 0; i < winningCombinations.length; i++) {
+      const [a, b, c] = winningCombinations[i];
+      if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+        return cells[a];
+      }
+    }
+    return null;
+  };
 
   const handleCellToggle = (index) => {
     const newCells = [...cells];
-    if (newCells[index] === null) {
+    if (newCells[index] === null && winner === null) {
+      // only allow moves if there is no winner
       newCells[index] = nextSymbol;
+      const newHistory = [...history, newCells]; // add the new state to the history
       setCells(newCells);
+      setHistory(newHistory);
+      setCurrentMove(newHistory.length - 1);
       setNextSymbol(nextSymbol === "❌" ? "🇴" : "❌");
+      setWinner(calculateWinner(newCells));
+    }
+  };
+
+  const handleGoBack = () => {
+    if (currentMove > 0) {
+      const newHistory = history.slice(0, currentMove); // remove the last state from the history
+      const prevBoard = newHistory[newHistory.length - 1]; // get the previous state
+      setCells(prevBoard);
+      setHistory(newHistory);
+      setCurrentMove(currentMove - 1); // set the current move to the previous state
+      setNextSymbol(nextSymbol === "❌" ? "🇴" : "❌");
+      setWinner(calculateWinner(prevBoard));
+    } else {
+      handleResetBoard();
+      setHistory([]);
+      setCurrentMove(0);
+      setNextSymbol("❌");
     }
   };
 
   return (
     <div className="container">
-      <div className="">
-        <ResetButton onClick={resetBoard}></ResetButton>
+      <div className="button-container">
+        <ResetButton onClick={handleResetBoard}></ResetButton>
+        <GoBackButton onClick={handleGoBack}></GoBackButton>
+      </div>
+      <div className="board">
+        {" "}
         <div className="cell-row">
           <Cell handleClick={() => handleCellToggle(0)} symbol={cells[0]} />
           <Cell handleClick={() => handleCellToggle(1)} symbol={cells[1]} />
@@ -40,6 +97,7 @@ export default function App() {
           <Cell handleClick={() => handleCellToggle(8)} symbol={cells[8]} />
         </div>
       </div>
+      {winner ? <div className="winner">{winner} WINS!</div> : null}
     </div>
   );
 }
--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,8 +1,13 @@
 import css from "./Cell.module.css";
 
 export const Cell = ({ handleClick, symbol }) => {
+  const hoverColorClass = symbol === null ? css["cell-green"] : css["cell-red"];
+
   return (
-    <button type="button" className={css.cell} onClick={handleClick}>
+    <button
+      type="button"
+      className={`${hoverColorClass} ${css.cell}`}
+      onClick={handleClick}>
       {symbol}
     </button>
   );
